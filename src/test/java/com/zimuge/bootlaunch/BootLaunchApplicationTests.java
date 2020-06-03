0cf5a549307b05d73cca49ed01362e09084ba5b1
yu <271468628@qq.com>
2020-06-03 23:44:28 +0800

    '1-3hello'

---
+++ src/test/java/com/zimuge/bootlaunch/BootLaunchApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zimuge.bootlaunch;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLaunchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
