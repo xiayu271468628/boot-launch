f31bfe1ba36983ae59976803e4174b0a79f07dab
“xia <271468628@qq.com>
2020-06-04 17:43:28 +0800

    2-3:使用常用注解开发RESTful接口

---
+++ src/main/java/com/zimuge/bootlaunch/controller/ArticleRestController.java
@@ -0,0 +1,11 @@
+package com.zimuge.bootlaunch.controller;
+
+/**
+ * @author ：xy
+ * @date ：Created in 2020/6/4 16:32
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class ArticleRestController {
+}
